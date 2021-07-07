9cc3115be2208ed9c5d5f14990cb47d0826c3acf
nirrozdata <87060389+nirrozdata@users.noreply.github.com>
2021-07-07 17:19:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import dtlpy as dl
+import os
+
+class ServiceRunner(dl.BaseServiceRunner):
+
+    def autoscaler_test_move(self, item, progress=None):
+        name, ext = os.path.splitext(item.filename)
+        new_name = name + "_moved" + ext
+        item.move(new_name)
+        return item.id
